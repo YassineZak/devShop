9e03d00d1cc244abb11b6ed21fdc367ec04e65da
Yassine <yassine.zakari@hotmail.fr>
2019-01-07 18:46:06 +0100

    ajout de tests unitaires

--- tests/YZ/SearchBundle/Controller/SearchControllerTest.php
+++ tests/YZ/SearchBundle/Controller/SearchControllerTest.php
@@ -10,6 +10,9 @@ namespace Tests\YZ\SearchBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use YZ\EcommerceBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 
 class SearchControllerTest extends WebTestCase
 {
@@ -17,6 +20,19 @@ class SearchControllerTest extends WebTestCase
         $client = static::createClient();
         $client->request('GET', '/search');
         $this->assertSame(200, $client->getResponse()->getStatusCode());
+    }
+    public function testJsonResultSearch(){
+
+        $term = 'xbox';
+        static::bootKernel();
+        $entityManager = static::$kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $repository = $entityManager->getRepository('YZEcommerceBundle:Product');
+        $searchresult = $repository->findByTerm($term);
+        $response = new JsonResponse();
+        $response->setData($searchresult);
+        dump($response);
+        die;
+        return $response;
     }
 
 }
